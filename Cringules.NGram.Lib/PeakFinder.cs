4d711a5cc1368fd6c775921c8a6195377195cf53
MilkyCode13 <shmayhel.andrey@gmail.com>
2023-03-05 16:31:03 +0300

    Add classes from app

---
+++ Cringules.NGram.Lib/PeakFinder.cs
@@ -0,0 +1,26 @@
+﻿using Cringules.NGram.Api;
+
+namespace Cringules.NGram.Lib;
+
+public class PeakFinder
+{
+    public static List<Peak> FindPeaks(PlotData? data)
+    {
+        var peaks = new List<Peak>();
+        if (data == null)
+        {
+            return peaks;
+        }
+        
+        for (var i = 1; i < data.Points.Count - 1; i++)
+        {
+            if (data.Points[i - 1].Intensity < data.Points[i].Intensity &&
+                data.Points[i + 1].Intensity < data.Points[i].Intensity)
+            {
+                peaks.Add(new Peak(data.Points[i].Angle, 0, data.Points[i].Intensity, 0));
+            }
+        }
+
+        return peaks;
+    }
+}
