7d35a56a65bf3f4ada46be59f98da186c112c43a
mariiamamedli <mariiamamedli@ya.ru>
2023-05-07 12:55:50 +0300

    fix XrayPeakAnalyzer

--- Cringules.NGram.Lib/PeakInfo.cs
+++ Cringules.NGram.Lib/PeakInfo.cs
@@ -43,7 +43,7 @@ public struct PeakInfo
     /// <summary>
     /// Межплоскостное расстояние.
     /// </summary>
-    public double InterplaneDistance { get; }
+    public double InterplanarDistance { get; }
 
     /// <summary>
     /// Конструктор структуры характеристик.
@@ -55,10 +55,10 @@ public struct PeakInfo
     /// <param name="integralWidth">Интегральная ширина.</param>
     /// <param name="peakWidth">Ширина отражения на половине высоты пика.</param>
     /// <param name="topAngle">Угол отражения.</param>
-    /// <param name="interplaneDistance">Межплоскостное расстояние.</param>
+    /// <param name="interplanarDistance">Межплоскостное расстояние.</param>
     public PeakInfo(double intensityMaximum, double intensityIntegral, double intensityApproximated,
         double intensityDifference, double integralWidth, double peakWidth, double topAngle,
-        double interplaneDistance)
+        double interplanarDistance)
     {
         IntensityMaximum = intensityDifference;
         IntensityIntegral = intensityIntegral;
@@ -67,6 +67,6 @@ public struct PeakInfo
         IntegralWidth = integralWidth;
         PeakWidth = peakWidth;
         TopAngle = topAngle;
-        InterplaneDistance = interplaneDistance;
+        InterplanarDistance = interplanarDistance;
     }
 }
